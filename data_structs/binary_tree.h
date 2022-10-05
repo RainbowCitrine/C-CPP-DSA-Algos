fa07f6880e93888859d3328564ee24711df18717
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-05 02:28:44 -0400

    Update binary_tree.h

--- data_structs/binary_tree.h
+++ data_structs/binary_tree.h
@@ -1,5 +1,6 @@
-//We also can use the term "node" for our binary tree, instead.
+//@Author: Armend Veseli.
 
+//We also can use the term "node" for our binary tree, instead.
 template <typename T>
 struct tree
 {
