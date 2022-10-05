f234656807b24dc1e08b9ea98b1e223e9982b882
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-05 02:23:37 -0400

    Create binary_tree.cpp

---
+++ data_structs/binary_tree.cpp
@@ -0,0 +1,16 @@
+#include "binary_tree.h"
+
+//A binary tree with chars.
+tree<char> my_tree;
+my_tree->left->right->left.value = 'c';
+
+/*
+A graphical representation of line 5:
+
+    root
+      /
+      \
+      /
+    'c'
+
+*/
